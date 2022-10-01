2a3156b63fa70770fcf253c4498577f06be99562
julqgan <110390472+julqgan@users.noreply.github.com>
2022-10-01 16:06:15 +0200

    finished lesson 6

--- src/app.js
+++ src/app.js
@@ -39,6 +39,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
