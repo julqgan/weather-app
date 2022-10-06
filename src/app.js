8950a809ac968986660c3084a4cfe6bd9e3ad77e
julqgan <110390472+julqgan@users.noreply.github.com>
2022-10-06 20:47:40 +0200

    Added function to JS

--- src/app.js
+++ src/app.js
@@ -21,6 +21,28 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayforecast() {
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+              <div class="col-2">
+                <div class="weather-forcast-date"> ${day} </div>
+                  <img src="http://openweathermap.org/img/wn/50d@2x.png" alt="" width="42"/>
+<div class="weather-forecast-temperatures">
+  <span class="weather-forecast-temperature-max"> 18° </span>
+  <span class="weather-forecast-temperature-min"> 12° </span>
+</div>
+                </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -88,3 +110,5 @@ let celsiuslink = document.querySelector("#celsius-link");
 celsiuslink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+
+displayforecast();
